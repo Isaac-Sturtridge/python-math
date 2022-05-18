ea5e2e603296f71c6bebd72a1a0016a9443a3c0e
Isaac Sturtridge <eyes330@yahoo.co.uk>
2022-05-18 18:40:54 +0800

    two new maths files

--- EvenOrOddGenerator.py
+++ EvenOrOddGenerator.py
@@ -1,23 +1,28 @@
 #even-odd vending machine
 
-number = int(input("Enter a number: "))
+number = float(input("Enter an integer: "))
 
-if number % 2 == 0:
-    print(f"{number} is even.")
-    print("Here are the next ten even numbers: ", end="")
-    for num in range(9):
-        if num == 8:
-            print(str(number) + ".")
-        else:
-            print(str(number) + ",", end=" ")
-        number += 2
+if number.is_integer() == False:
+    print("Please enter an integer")
 
 else:
-    print(f"{number} is odd.")
-    print("Here are the next ten odd numbers: ", end="")
-    for num in range(9):
-        if num == 8:
-            print(str(number) + ".")
-        else:
-            print(str(number) +  ",", end=" ")
-        number += 2
+    number = int(number)
+
+    if number % 2 == 0:
+        print(f"{number} is even.")
+        print("Here are the next ten even numbers: ", end="")
+        for num in range(9):
+            if num == 8:
+                print(str(number) + ".")
+            else:
+                print(str(number) + ",", end=" ")
+            number += 2
+    else:
+        print(f"{number} is odd.")
+        print("Here are the next ten odd numbers: ", end="")
+        for num in range(9):
+            if num == 8:
+                print(str(number) + ".")
+            else:
+                print(str(number) +  ",", end=" ")
+            number += 2
---
+++ MultiplicationTableExtended.py
@@ -0,0 +1,21 @@
+# multiplication table
+
+
+# enter as floats to allow float and integer input without error
+table = float(input('Enter a number: '))
+index = float(input('Enter the number of multiplications to display: '))
+
+def multi_table(table):
+    for i in range(1, int(index) + 1):
+         print('{0} x {1} = {2}'.format(table, i, table*i))
+
+if index.is_integer() == False:
+    print("Please enter an integer when choosing how many to display")
+
+else:
+    # converts table to integer if applicable to make it look nicer
+    if table.is_integer() == True:
+        table = int(table)
+    index = int(index)
+    multi_table(table) 
+
